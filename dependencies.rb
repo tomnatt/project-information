236d38ebb154859843a2871943bdb3667d98ff8e
tomnatt <github@tomnatt.com>
2017-05-27 19:58:53 +0100

    Get repos for a user from GitHub API
    Get all repos and pull dependencies from libraries.io
    
    Currently filters to Ruby projects only

--- dependencies.rb
+++ dependencies.rb
@@ -1,12 +1,25 @@
 require 'httparty'
 
-url = 'https://libraries.io/api/github/tomnatt/monzoapp/dependencies?api_key=' + ENV['LIBRARIES_API_KEY']
+github_repos_url = 'https://api.github.com/users/tomnatt/repos'
+response_json = HTTParty.get(github_repos_url).body
 
-response_json = HTTParty.get(url).body
-dependencies = JSON.parse(response_json)['dependencies']
+# Skip everything except ruby for now
+repos = JSON.parse(response_json).map { |repo| repo['full_name'] if repo['language'] == 'Ruby' }
+repos.reject!(&:nil?)
 
-printf "%-20s\t\t%s\t\t%s\n", 'dependency', 'current', 'latest'
-dependencies.each do |dep|
-  next unless dep['filepath'] == 'Gemfile.lock' && dep['outdated']
-  printf "%-20s\t\t%s\t\t%s\n", dep['project_name'], dep['requirements'], dep['latest_stable']
+repos.each do |repo_name|
+  libraries_url = "https://libraries.io/api/github/#{repo_name}/dependencies?api_key=#{ENV['LIBRARIES_API_KEY']}"
+
+  response_json = HTTParty.get(libraries_url).body
+  dependencies = JSON.parse(response_json)['dependencies']
+
+  next unless dependencies
+
+  puts repo_name
+  printf "%-20s\t\t%s\t\t%s\n", 'dependency', 'current', 'latest'
+  dependencies.each do |dep|
+    next unless dep['filepath'] == 'Gemfile.lock' && dep['outdated']
+    printf "%-20s\t\t%s\t\t%s\n", dep['project_name'], dep['requirements'], dep['latest_stable']
+  end
+  puts ''
 end
